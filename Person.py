e42a1a5a8e7822b9a790721d82901e7808f398e3
changoo <cck742@naver.com>
2024-10-10 23:19:07 +0900

    comment modified

--- Person.py
+++ Person.py
@@ -6,12 +6,12 @@ class Person:
         self.gender = gender
         self.age = int(age)
 
-    def display(self):  # 메서드 정의
+    def display(self): # 최종 출력
         print(f"\n이름 : {self.name}, 성별: {self.gender}")
         print(f"나이 : {self.age}")
         self.greet()
         
-    def greet(self):
+    def greet(self): # 인사 메시지
         if(self.age > 40):
             print(f"안녕하시렵니까, {self.name}! 어른이시군요!")
         elif(self.age > 20):
@@ -19,7 +19,7 @@ class Person:
         else:
             print(f"안녕, {self.name}! 응애군요!") 
             
-def get_age():
+def get_age(): # 나이 입력 1이상
     while True:
         try:
             age = int(input("나이 : "))
@@ -30,7 +30,7 @@ def get_age():
         except ValueError:
             print("잘못된 나이를 입력하셨습니다.")
     
-def get_name():
+def get_name(): # 이름 입력 한글 or 영어
     name = input("이름 : ").strip()
     pattern_en = r'^[a-zA-Z\s]+$'   # 영어 및 공백만 허용
     pattern_kr = r'^[가-힣\s]+$'     # 한글 및 공백만 허용
@@ -40,7 +40,7 @@ def get_name():
         print("잘못된 이름을 입력하셨습니다.")
         return get_name()
     
-def get_gender():
+def get_gender(): # 성별 입력 male or female
     gender = input("성별 : ").strip().lower()
     
     if gender in ['male', 'female']:
